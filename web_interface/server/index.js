29af0f18fd7fe7e5e9d1da67b16893680035300d
TheMapleTr3 <yanqiu@student.unimelb.edu.au>
2022-02-27 14:16:31 +1000

    added web interface

---
+++ web_interface/server/index.js
@@ -0,0 +1,97 @@
+const express = require('express')
+const request = require('request');
+const dotenv = require('dotenv');
+const {spawn} = require('child_process');
+
+const port = 5000
+
+global.access_token = ''
+
+dotenv.config()
+
+var spotify_client_id = process.env.SPOTIFY_CLIENT_ID
+var spotify_client_secret = process.env.SPOTIFY_CLIENT_SECRET
+
+var spotify_redirect_uri = 'http://localhost:3000/auth/callback'
+
+var generateRandomString = function (length) {
+  var text = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+
+  for (var i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+var app = express();
+
+app.get('/auth/login', (req, res) => {
+
+  var scope = "streaming user-read-email user-read-private"
+  var state = generateRandomString(16);
+
+  var auth_query_parameters = new URLSearchParams({
+    response_type: "code",
+    client_id: spotify_client_id,
+    scope: scope,
+    redirect_uri: spotify_redirect_uri,
+    state: state
+  })
+
+  res.redirect('https://accounts.spotify.com/authorize/?' + auth_query_parameters.toString());
+})
+
+app.get('/auth/callback', (req, res) => {
+
+  var code = req.query.code;
+
+  var authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    form: {
+      code: code,
+      redirect_uri: spotify_redirect_uri,
+      grant_type: 'authorization_code'
+    },
+    headers: {
+      'Authorization': 'Basic ' + (Buffer.from(spotify_client_id + ':' + spotify_client_secret).toString('base64')),
+      'Content-Type' : 'application/x-www-form-urlencoded'
+    },
+    json: true
+  };
+
+  request.post(authOptions, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      access_token = body.access_token;
+      res.redirect('/')
+    }
+  });
+
+})
+
+app.get('/auth/token', (req, res) => {
+  res.json({ access_token: access_token})
+})
+
+// app.get('/auth/pythonAPI', (req, res) => {
+ 
+//   var dataToSend;
+//   // spawn new child process to call the python script
+//   const python = spawn('python', ['dataInput.py']);
+//   // collect data from script
+//   python.stdout.on('data', function (data) {
+//    console.log('Pipe data from python script ...');
+//    dataToSend = data.toString();
+//   });
+//   // in close event we are sure that stream from child process is closed
+//   python.on('close', (code) => {
+//   console.log(`child process close all stdio with code ${code}`);
+//   // send data to browser
+//   res.send(dataToSend)
+//   });
+  
+//  })
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`)
+})
---
+++ web_interface/src/WebPlayback.jsx
@@ -0,0 +1,265 @@
+import React, { useState, useEffect } from 'react';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import { fontSize } from '@mui/system';
+
+const track = {
+    name: "",
+    album: {
+        images: [
+            { url: "" }
+        ]
+    },
+    artists: [
+        { name: "" }
+    ]
+}
+
+
+
+function generate(element) {
+    return [0, 1, 2, 3,4].map((value) =>
+      React.cloneElement(element, {
+        key: value,
+      })
+    );
+}
+
+const title = {
+    color: "white",
+    fontSize:20
+};
+
+const text = {
+    color: "white",
+    fontSize:14
+};
+
+function WebPlayback(props) {
+
+    const [is_paused, setPaused] = useState(false);
+    const [is_active, setActive] = useState(false);
+    const [showElement, setShowElement] = useState(true);
+    const [player, setPlayer] = useState(undefined);
+    const [current_track, setTrack] = useState(track);
+
+
+    useEffect(() => {
+
+        const script = document.createElement("script");
+        script.src = "https://sdk.scdn.co/spotify-player.js";
+        script.async = true;
+
+        document.body.appendChild(script);
+
+        window.onSpotifyWebPlaybackSDKReady = () => {
+
+            const player = new window.Spotify.Player({
+                name: 'Web Playback SDK',
+                getOAuthToken: cb => { cb(props.token); },
+                volume: 0.5
+            });
+
+            setPlayer(player);
+
+            player.addListener('ready', ({ device_id }) => {
+                console.log('Ready with Device ID', device_id);
+            });
+
+            player.addListener('not_ready', ({ device_id }) => {
+                console.log('Device ID has gone offline', device_id);
+            });
+
+            player.addListener('player_state_changed', ( state => {
+
+                if (!state) {
+                    return;
+                }
+                
+                setTrack(state.track_window.current_track);
+                setPaused(state.paused);
+
+                player.getCurrentState().then( state => { 
+                    (!state)? setActive(false) : setActive(true) 
+                });
+
+            }));
+
+            setTimeout(function () {
+                setShowElement(false);
+              }, 10000);
+            
+
+            player.connect();
+
+        };
+    }, []);
+
+    if (!is_active) { 
+        return (
+            <>
+            {/* My internet went down, so have to hard code most of it because I can't use spotify api */}
+                <div className="container">
+                    <div className="main-wrapper">
+
+                        <img src={"https://i.scdn.co/image/ab67616d0000b273e8107e6d9214baa81bb79bba"}  className="now-playing__cover" alt="" />
+
+                        <div className="now-playing__side">
+                            <div className="now-playing__name">{"Happy - From \"Despicable Me 2\""}</div>
+                            <div className="now-playing__artist">{"Pharrell Williams"}</div>
+
+                            <button className="btn-spotify" onClick={() => { player.previousTrack() }} >
+                                &lt;&lt;
+                            </button>
+
+                            <button className="btn-spotify" onClick={() => { player.togglePlay() }} >
+                                { is_paused ? "PLAY" : "PAUSE" }
+                            </button>
+
+                            <button className="btn-spotify" onClick={() => { player.nextTrack() }} >
+                                &gt;&gt;
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {/* my code */}
+                <div><br></br></div>
+                <div className='container'>
+                    <h2>Queued</h2>
+                </div>
+
+                {showElement ? (
+                        <div className="list-container">
+                        <div className="main-list" >
+                            <List sx={{ width: '100%', maxWidth: 600 }}>
+                                    <ListItem>
+                                        <img src={"https://i.scdn.co/image/ab67616d0000b273e419ccba0baa8bd3f3d7abf2"} className="next-up__cover" alt="" />
+                                        <ListItemText primaryTypographyProps={{ style: title }} primary={"Uptown Funk (feat. Bruno Mars)"} secondaryTypographyProps={{ style: text }} secondary={"Mark Ronson, Bruno Mars"} />
+                                    </ListItem>
+                                    <ListItem>
+                                        <img src={"https://i.scdn.co/image/ab67616d0000b273ef37416970812293c08e8a78"} className="next-up__cover" alt="" />
+                                        <ListItemText primaryTypographyProps={{ style: title }} primary={"CAN'T STOP THE FEELING! (from DreamWorks Animation's \"TROLLS\")"} secondaryTypographyProps={{ style: text }} secondary={"Justin Timberlake"} />
+                                    </ListItem>
+                                    <ListItem>
+                                        <img src={"https://i.scdn.co/image/ab67616d0000b27352b2a3824413eefe9e33817a"} className="next-up__cover" alt="" />
+                                        <ListItemText primaryTypographyProps={{ style: title }} primary={"Shake It Off"} secondaryTypographyProps={{ style: text }} secondary={"Taylor Swift"} />
+                                    </ListItem>
+                                    <ListItem>
+                                        <img src={"https://i.scdn.co/image/ab67616d0000b273e8dd4db47e7177c63b0b7d53"} className="next-up__cover" alt="" />
+                                        <ListItemText primaryTypographyProps={{ style: title }} primary={"Take on Me"} secondaryTypographyProps={{ style: text }} secondary={"a-ha"} />
+                                    </ListItem>
+                                    <ListItem>
+                                        <img src={"https://i.scdn.co/image/ab67616d0000b273240447f2da1433d8f4303596"} className="next-up__cover" alt="" />
+                                        <ListItemText primaryTypographyProps={{ style: title }} primary={"Butter"} secondaryTypographyProps={{ style: text }} secondary={"BTS"} />
+                                    </ListItem>
+                            </List>
+                        </div>
+                        </div>
+                    ) : (
+                        <div className="list-container">
+                        <div className="main-list" >
+                            <List sx={{ width: '100%', maxWidth: 600 }}>
+                                {/* sad songs */}
+                                <ListItem>
+                                    <img src={"https://i.scdn.co/image/ab67616d0000b27312549da864353c084cf0faa6"} className="next-up__cover" alt="" />
+                                    <ListItemText primaryTypographyProps={{ style: title }} primary={"Cross Road Blues"} secondaryTypographyProps={{ style: text }} secondary={"Robert Johnson"} />
+                                </ListItem>
+                                <ListItem>
+                                    <img src={"https://i.scdn.co/image/ab67616d0000b273599bb610f6243326e6176663"} className="next-up__cover" alt="" />
+                                    <ListItemText primaryTypographyProps={{ style: title }} primary={"Born Under A Bad Sign"} secondaryTypographyProps={{ style: text }} secondary={"Albert King"} />
+                                </ListItem>
+                                <ListItem>
+                                    <img src={"https://i.scdn.co/image/ab67616d0000b273621489fd90d50158b3714cb0"} className="next-up__cover" alt="" />
+                                    <ListItemText primaryTypographyProps={{ style: title }} primary={"Mad World (Feat. Michael Andrews)"} secondaryTypographyProps={{ style: text }} secondary={"Gary Jules, Michael Andrews"} />
+                                </ListItem>
+                                <ListItem>
+                                    <img src={"https://i.scdn.co/image/ab67616d0000b273126363a3b84cec538f5e687f"} className="next-up__cover" alt="" />
+                                    <ListItemText primaryTypographyProps={{ style: title }} primary={"Memphis Blues"} secondaryTypographyProps={{ style: text }} secondary={"Jack Dupree, Speckled Red, Memphis Slim, Sunnyland Slim, Eddie Boyd, Little Brother Montgomery"} />
+                                </ListItem>
+                                <ListItem>
+                                    <img src={"https://i.scdn.co/image/ab67616d0000b27390afd8e4ec6d787114ed6c40"} className="next-up__cover" alt="" />
+                                    <ListItemText primaryTypographyProps={{ style: title }} primary={"The Scientist"} secondaryTypographyProps={{ style: text }} secondary={"Coldplay"} />
+                                </ListItem>
+                            </List>
+                        </div>
+                        </div>
+                )};
+                
+            
+            </>)
+    } else {
+        return (
+            <>
+                <div className="container">
+                    <div className="main-wrapper">
+
+                        <img src={current_track.album.images[0].url} className="now-playing__cover" alt="" />
+
+                        <div className="now-playing__side">
+                            <div className="now-playing__name">{current_track.name}</div>
+                            <div className="now-playing__artist">{current_track.artists[0].name}</div>
+
+                            <button className="btn-spotify" onClick={() => { player.previousTrack() }} >
+                                &lt;&lt;
+                            </button>
+
+                            <button className="btn-spotify" onClick={() => { player.togglePlay() }} >
+                                { is_paused ? "PLAY" : "PAUSE" }
+                            </button>
+
+                            <button className="btn-spotify" onClick={() => { player.nextTrack() }} >
+                                &gt;&gt;
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                {/* my code */}
+                <div><br></br></div>
+                <div className='container'>
+                    <h2>Queued</h2>
+                </div>
+                <div className="list-container">
+                    <div className="main-list" >
+                        <List sx={{ width: '100%', maxWidth: 600 }}>
+                            <ListItem>
+                                <img src={current_track.album.images[0].url} className="next-up__cover" alt="" />
+                                <ListItemText primaryTypographyProps={{ style: title }} primary={current_track.name} secondaryTypographyProps={{ style: text }} secondary={current_track.artists[0].name} />
+                            </ListItem>
+                            <ListItem>
+                                <img src={current_track.album.images[0].url} className="next-up__cover" alt="" />
+                                <ListItemText primaryTypographyProps={{ style: title }} primary={current_track.name} secondaryTypographyProps={{ style: text }} secondary={current_track.artists[0].name} />
+                            </ListItem>
+                            <ListItem>
+                                <img src={current_track.album.images[0].url} className="next-up__cover" alt="" />
+                                <ListItemText primaryTypographyProps={{ style: title }} primary={current_track.name} secondaryTypographyProps={{ style: text }} secondary={current_track.artists[0].name} />
+                            </ListItem>
+                            <ListItem>
+                                <img src={current_track.album.images[0].url} className="next-up__cover" alt="" />
+                                <ListItemText primaryTypographyProps={{ style: title }} primary={current_track.name} secondaryTypographyProps={{ style: text }} secondary={current_track.artists[0].name} />
+                            </ListItem>
+                            <ListItem>
+                                <img src={current_track.album.images[0].url} className="next-up__cover" alt="" />
+                                <ListItemText primaryTypographyProps={{ style: title }} primary={current_track.name} secondaryTypographyProps={{ style: text }} secondary={current_track.artists[0].name} />
+                            </ListItem>
+                            {/* <List>
+                                {generate(
+                                    <ListItem>
+                                        <img src={current_track.album.images[0].url} className="next-up__cover" alt="" />
+                                    <ListItemText
+                                        primaryTypographyProps={{ style: title }} primary={next_track.name} secondaryTypographyProps={{ style: text }} secondary={next_track.artists[0].name} 
+
+                                    />
+                                    </ListItem>
+                                )}
+                            </List> */}
+
+                        </List>
+                    </div>
+                </div>
+            </>
+        );
+    }
+}
+
+export default WebPlayback
